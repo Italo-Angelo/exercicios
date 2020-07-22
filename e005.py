9ffb77993e08891381c303f95eea242c1849deed
ItaloAngelodosSantos <karu123okumura@gmail.com>
2020-07-22 11:31:20 -0300

    e005

---
+++ e005.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número e iremos que deseja saber seu sucessor e antecessor: '))
+n1 = n -1 
+n2 = n + 1
+print('O antecessor de {} é {}, e seu sucessor é {}'.format(n, n1, n2))
